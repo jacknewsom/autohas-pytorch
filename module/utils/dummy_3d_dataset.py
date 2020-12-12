1fabdf6e4a911ba7228529a92a68108d712ea4d3
Jack Newsom <jackmnewsom@gmail.com>
2020-12-12 15:45:42 -0800

    added LArPose dataset

--- module/utils/dummy_3d_dataset.py
+++ module/utils/dummy_3d_dataset.py
@@ -1,3 +1,5 @@
+from scipy.stats import multivariate_normal
+from itertools import product
 import torch
 import numpy as np
 
@@ -17,3 +19,45 @@ class DummyData(torch.utils.data.Dataset):
         centroid = np.random.rand(3,)*2 -1
 
         return active, centroid
+
+class DummyPose(torch.utils.data.Dataset):
+    def __init__(self, length, seed=42, load_torch=True):
+        self.length = length
+        self.seed = seed
+        self.volume = [100, 100, 100]
+        # return as tensor?
+        self.load_torch = load_torch
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, i):
+        '''
+        Create something like a muon track, but target is sparse
+        heatmaps
+        '''
+        if i < 0 or i >= self.length:
+            i %= self.length
+
+        np.random.seed(i*self.seed)
+        start, end = np.random.randint(0, self.volume, (2, 3))
+        distance = np.linalg.norm(start-end).astype(int)
+
+        locations = np.linspace(start, end, num=distance).astype(int)
+        features = np.ones((len(locations), 1))
+
+        # evaluate standard multivariate normal on grid
+        densities = np.zeros(len(locations))
+        for point in start, end:
+            gaussian = multivariate_normal(point, [1 for i in self.volume])
+            densities += gaussian.pdf(locations)
+        densities /= 2
+
+        densities = densities.reshape(-1, 1)
+
+        if self.load_torch:
+            locations = torch.from_numpy(locations).long()
+            features = torch.from_numpy(features).float()
+            densities = torch.from_numpy(densities).float()
+
+        return locations, features, densities
--- module/utils/muon_track_dataset.py
+++ module/utils/muon_track_dataset.py
@@ -1,3 +1,5 @@
+from scipy.stats import multivariate_normal
+from itertools import product
 import os
 import torch
 import h5py as five
@@ -62,3 +64,81 @@ class MuonTracks(torch.utils.data.Dataset):
                         continue
                     sample[key] = sample[key][::int(len(sample[key])/self.MAX_SIZE)]
         return sample
+
+class MuonPose(torch.utils.data.Dataset):
+    '''
+    Similar to MuonTracks, but for predicting heatmaps instead of points directly
+    '''
+
+    def __init__(self, data_dir, heatmap_size=9, output_shape=[128, 128, 128], dense_target=False):
+        super(MuonPose, self).__init__()
+        if not os.path.isdir(data_dir):
+            raise OSError("Directory %s does not exist" % data_dir)
+        if data_dir[-1] != '/':
+            data_dir += '/'
+        self.data_dir = data_dir
+        self.files = [self.data_dir+f for f in os.listdir(data_dir) if f.endswith('.hdf5')]
+        self.default_keys = ['energy_coordinates', 'sipm_coordinates', 'sipm_values',]
+        self.heatmap_size = heatmap_size
+        self.output_shape = output_shape
+        self.dense_target = dense_target
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, i):
+        '''
+        args:
+            - i (int): `i`th item from dataset to load
+
+        return:
+        '''
+        assert type(i) == int, f"Argument `i` must be an int, but {type(i)} was provided"
+
+        with five.File(self.files[i], 'r') as f:
+            np.random.seed(i)
+            sample = {k: f[k][:] for k in self.default_keys}
+
+            locations, features = sample['sipm_coordinates'], sample['sipm_values'].reshape(-1, 1)
+            locations = (locations * np.array([128/1000, 128/3000, 128/1000])).astype(int)
+
+            energy_coordinates = sample['energy_coordinates']
+
+            # rescale to be in region (128, 128, 128)
+            start, end = energy_coordinates[0], energy_coordinates[-1]
+            start = (start * np.array([128/1000, 128/3000, 128/1000])).astype(int)
+            end = (end * np.array([128/1000, 128/3000, 128/1000])).astype(int)
+
+            if not self.dense_target:
+                def cartesian_product(*arrays):
+                    la = len(arrays)
+                    dtype = np.result_type(*arrays)
+                    arr = np.empty([len(a) for a in arrays] + [la], dtype=dtype)
+                    for i, a in enumerate(np.ix_(*arrays)):
+                        arr[...,i] = a
+                    return arr.reshape(-1, la)
+                pts = np.arange(128)
+                heatmap = cartesian_product(pts, pts, pts)
+                densities = np.zeros(len(heatmap))
+
+                mvn1 = multivariate_normal(start, [1,1,1])
+                mvn2 = multivariate_normal(end, [1,1,1])
+
+                densities += mvn1.pdf(heatmap)
+                densities += mvn2.pdf(heatmap)
+                densities /= densities.sum()
+
+                heatmap, densities = heatmap[densities>0], densities[densities>0]
+
+                return (locations, features), (heatmap, densities)
+            else:
+                mvn1 = multivariate_normal(start, [1,1,1])
+                mvn2 = multivariate_normal(end, [1,1,1])
+                target = np.zeros([128, 128, 128])
+                for i in range(128):
+                    for j in range(128):
+                        for k in range(128):
+                            x = np.array([i, j, k])
+                            target[i, j, k] += mvn1.pdf(x-start) + mvn2.pdf(x-end)
+                target /= 2
+                return (locations, features), target
